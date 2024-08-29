04247c6b890b91aa13470f4815d2ef169eae83ea
Andrii Petrovskyi <andrii.petrovskyi@globallogic.com>
2024-08-29 14:30:39 +0300

    finished admin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const Homepage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('admin')
+  }, [])
   return (
     <div className=''>Homepage</div>
   )
